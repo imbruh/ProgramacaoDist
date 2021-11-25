5dd9ce0dd988fceb280dbc1149cfd9f9a7d4e256
imbruh <brunaluiza112001@gmail.com>
2021-11-25 20:50:06 -0300

    Pratica Roteamento de filas RabbitMQ

---
+++ RoteamentoFilasRabbitMQ/src/main/java/edu/br/roteamento/filas/rabbitmq/RoteamentoFilasRabbitMQ/RoteamentoFilasRabbitMqApplication.java
@@ -0,0 +1,13 @@
+package edu.br.roteamento.filas.rabbitmq.RoteamentoFilasRabbitMQ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoteamentoFilasRabbitMqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoteamentoFilasRabbitMqApplication.class, args);
+	}
+
+}
---
+++ RoteamentoFilasRabbitMQ/src/test/java/edu/br/roteamento/filas/rabbitmq/RoteamentoFilasRabbitMQ/RoteamentoFilasRabbitMqApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.br.roteamento.filas.rabbitmq.RoteamentoFilasRabbitMQ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoteamentoFilasRabbitMqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
