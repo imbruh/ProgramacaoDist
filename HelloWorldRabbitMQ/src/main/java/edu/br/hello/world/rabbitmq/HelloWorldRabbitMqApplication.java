f6982027e7e055a7f94738af34619d27567210d2
imbruh <brunaluiza112001@gmail.com>
2021-11-25 16:45:23 -0300

    Prática Olá Mundo com RabbitMQ

---
+++ HelloWorldRabbitMQ/src/main/java/edu/br/hello/world/rabbitmq/HelloWorldRabbitMqApplication.java
@@ -0,0 +1,13 @@
+package edu.br.hello.world.rabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldRabbitMqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldRabbitMqApplication.class, args);
+	}
+
+}
---
+++ HelloWorldRabbitMQ/src/test/java/edu/br/hello/world/rabbitmq/HelloWorldRabbitMqApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.br.hello.world.rabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldRabbitMqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
