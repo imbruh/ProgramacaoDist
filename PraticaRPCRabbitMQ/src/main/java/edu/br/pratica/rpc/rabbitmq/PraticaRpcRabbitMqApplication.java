cbc52782089a278aca45039f091240aeb09cb97d
imbruh <brunaluiza112001@gmail.com>
2021-11-25 17:12:26 -0300

    Pratica RPC RabbitMQ

---
+++ PraticaRPCRabbitMQ/src/main/java/edu/br/pratica/rpc/rabbitmq/PraticaRpcRabbitMqApplication.java
@@ -0,0 +1,13 @@
+package edu.br.pratica.rpc.rabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraticaRpcRabbitMqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PraticaRpcRabbitMqApplication.class, args);
+	}
+
+}
---
+++ PraticaRPCRabbitMQ/src/test/java/edu/br/pratica/rpc/rabbitmq/PraticaRpcRabbitMqApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.br.pratica.rpc.rabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraticaRpcRabbitMqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
